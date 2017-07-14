5e2e89005a49f508148b2f3e79b8d9d5f99fd229
TheMisterPanek <mrPanek@yandex.by>
2017-07-14 18:13:00 +0300

    Add model and views for manuals and category.

---
+++ app/models/manual.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: manuals
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  category_id :string
+#  user_id     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Manual < ApplicationRecord
+  validates :name, :category_id, :user_id, presence: true
+  belongs_to :category
+end
