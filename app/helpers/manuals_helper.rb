c8d7ded9d8e03b2f73506b0416d838f59a13c173
Kirill Zaharenkov <zaharenkovkirill@gmail.com>
2017-07-23 13:30:13 +0300

    Fix code style.

--- app/helpers/manuals_helper.rb
+++ app/helpers/manuals_helper.rb
@@ -1,6 +1,5 @@
 module ManualsHelper
   def manual_json(manual)
-    { manual_id: manual.id, pages: manual.pages.to_a, current_page: 0}.to_json
+    { manual_id: manual.id, pages: manual.pages.to_a, current_page: 0 }.to_json
   end
-
 end
