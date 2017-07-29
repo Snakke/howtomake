73fe6f0d3631e04dad0f557f956f15ee6fb0cc0d
Kirill Zaharenkov <zaharenkovkirill@gmail.com>
2017-07-29 17:57:00 +0300

    Configure action cable for heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.mount_path = nil
+  config.action_cable.url = "wss://howtomake.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://howtomake.herokuapp.com', 'http://howtomake.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
