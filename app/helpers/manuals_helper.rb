829812da3acb46a87d1de343ecd07a5f5b7c5265
Kirill Zaharenkov <zaharenkovkirill@gmail.com>
2017-07-31 11:35:28 +0300

    Configure views. Add footer.

--- app/helpers/manuals_helper.rb
+++ app/helpers/manuals_helper.rb
@@ -1,5 +1,5 @@
 module ManualsHelper
   def manual_data(manual)
-    { manual_id: manual.id, pages: manual.pages, current_page: 0 , edit_mode: false}
+    { manual_id: manual.id, pages: manual.pages, current_page: 0, edit_mode: false }
   end
 end
