31e5e87f8c224ef5ec09576d8cb9f4c6ff384ef3
Kirill Zaharenkov <zaharenkovkirill@gmail.com>
2017-08-03 13:50:06 +0300

    Add tag cloud.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,6 @@
 class HomeController < ApplicationController
-  helper_method :sort_column, :sort_direction
-
   def index
     @new_manuals = Manual.order(created_at: :desc).limit(5)
-  end
-
-  def tag_cloud
     @tags = Manual.tag_counts_on(:tags)
-  end
-
-  private
-
-  def sort_column
-    Manual.column_names.include?(params[:sort]) ? params[:sort] : 'title'
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
   end
 end
