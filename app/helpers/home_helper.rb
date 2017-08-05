c4efc9001070ffb8e8a83ded37e93f2be1cac7ef
Kirill Zaharenkov <zaharenkovkirill@gmail.com>
2017-08-05 16:10:45 +0300

    Fix code style.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
   include ActsAsTaggableOn::TagsHelper
   def manual_rating(rating)
-    return rating if rating>0
+    return rating if rating > 0
     t('view.not_rated')
   end
 end
--- db/migrate/20170804192603_change_default_image_in_users.rb
+++ db/migrate/20170804192603_change_default_image_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultImageInUsers < ActiveRecord::Migration[5.1]
   def change
-    change_column_default :users, :image, "default/default-image.jpg"
+    change_column_default :users, :image, 'default/default-image.jpg'
   end
 end
